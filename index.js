07196837a52c24b0aa5791fc73860602701c08f1
kkandil <khaled.kandil.per@gmail.com>
2023-12-25 13:34:54 +0100

    first commit

--- index.js
+++ index.js
@@ -24,50 +24,50 @@
 
 ////////////////////////////////////////////////////////////////////////////////////////////
 // socket.IO
-const express = require('express');
-var app = express();
-//var server = require('http').createServer();
-var server = require('http').createServer(app);
-const io = require('socket.io')(server);
-const PORT = process.env.PORT || 3000;
+// const express = require('express');
+// var app = express();
+// //var server = require('http').createServer();
+// var server = require('http').createServer(app);
+// const io = require('socket.io')(server);
+// const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-	//res.write('<h1>Socket IO Start on port: ${PORT}</h1>');
-	//res.end();
-	res.send("Socket IO Start on port: " + PORT);
-});
+// app.get('/', (req, res) => {
+// 	//res.write('<h1>Socket IO Start on port: ${PORT}</h1>');
+// 	//res.end();
+// 	res.send("Socket IO Start on port: " + PORT);
+// });
 
-server.listen(PORT, function(){
-	console.log("Server is now running...");
-});
+// server.listen(PORT, function(){
+// 	console.log("Server is now running...");
+// });
 
-//io.on('connection', function(socket){
-io.on("connection", (socket) => {
-	console.log("client connected...");
+// //io.on('connection', function(socket){
+// io.on("connection", (socket) => {
+// 	console.log("client connected...");
 
-	socket.on("connect_error", (err) => {
-		console.log(`connect_error due to ${err.message}`);
-	  });
+// 	socket.on("connect_error", (err) => {
+// 		console.log(`connect_error due to ${err.message}`);
+// 	  });
 
-	socket.on('message', function(data) {
-		io.emit('message', "Server say hi");
-		console.log('received: %s', data);
-	});
+// 	socket.on('message', function(data) {
+// 		io.emit('message', "Server say hi");
+// 		console.log('received: %s', data);
+// 	});
 
-	socket.on('disconnect', function(){
+// 	socket.on('disconnect', function(){
 		
-		console.log("Player Disconnected: " + socket.id);
-		/*
-		for(var i = 0; i < players.length; i++){
-			if(players[i].id == socket.id){
-				players.splice(i, 1);
-			}
-		}
-		*/
-	});
-});
+// 		console.log("Player Disconnected: " + socket.id);
+// 		/*
+// 		for(var i = 0; i < players.length; i++){
+// 			if(players[i].id == socket.id){
+// 				players.splice(i, 1);
+// 			}
+// 		}
+// 		*/
+// 	});
+// });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+// setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
 
 ////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -75,36 +75,36 @@ setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
 ////////////////////////////////////////////////////////////////////////////////////////////
 // WS
 
-// const PORT = process.env.PORT || 3000;
-// const INDEX = '/index.html';
-// const express = require("express");
-// const server = express()
-//   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-//   .listen(PORT, () => console.log(`Listening on ${PORT}`));
-// const { Server } = require('ws');
-// const wss = new Server({ server });
-// var parm1;
-// var parm2;
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+const express = require("express");
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+const { Server } = require('ws');
+const wss = new Server({ server });
+var parm1;
+var parm2;
 
-// wss.on('connection', (ws) => {
-// 	console.log('Client connected');
-// 	ws.on('close', () => console.log('Client disconnected'));
+wss.on('connection', (ws) => {
+	console.log('Client connected');
+	ws.on('close', () => console.log('Client disconnected'));
 
-// 	ws.on('message', function(data) {
-// 		parm1 = data['p1'];
-// 		parm2 = data['p2'];
+	ws.on('message', function(data) {
+		parm1 = data['p1'];
+		parm2 = data['p2'];
 
-// 		console.log('received: %s', data);
-// 		console.log('p1: %s', parm1);
-// 		console.log('p2: %s', parm2);
-// 		//ws.send('message', "Server say hi");
-// 	});
+		console.log('received: %s', data);
+		console.log('p1: %s', parm1);
+		console.log('p2: %s', parm2);
+		//ws.send('message', "Server say hi");
+	});
 
-// 	ws.on('message2', function message(data) {
-// 		console.log('received2: %s', data);
-// 		//ws.send('message', "Server say hi");
-// 	});
-//   });
+	ws.on('message2', function message(data) {
+		console.log('received2: %s', data);
+		ws.send('message', "Server say hi");
+	});
+  });
 
 ////////////////////////////////////////////////////////////////////////////////////////////
 
